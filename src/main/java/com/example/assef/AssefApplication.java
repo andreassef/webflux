c83dfefffefd69358045dce2aef96975b508f7e0
Andre Assef <andreassef02@gmail.com>
2021-11-09 16:53:01 -0300

    [ UPDATE ] - Ajustes na funcao de entrada

--- src/main/java/com/example/assef/AssefApplication.java
+++ src/main/java/com/example/assef/AssefApplication.java
@@ -1,12 +1,18 @@
 package com.example.assef;
 
+import com.example.assef.model.Movie;
+import com.example.assef.repository.NetflixRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @SpringBootApplication
@@ -21,4 +27,16 @@ public class AssefApplication {
 		return String.format("Hello %s!", name);
 	}
 
+	@Bean
+	CommandLineRunner init(ReactiveMongoOperations operations, NetflixRepository repository) {
+		return args -> {
+			Flux<Movie>  movieFlux = Flux.just(
+					new Movie(null, "Matrix", "Acao", "Keanu Reaves", 1995),
+					new Movie(null, "Diario de uma paixao", "Romance", "Ryan Gosling", 2003),
+					new Movie(null, "1987", "Drama", "George Mackay", 2020)
+			).flatMap(repository::save);
+
+			movieFlux.thenMany(repository.findAll()).subscribe(System.out::println);
+		};
+	}
 }
