81377cd0d08bbfa9c9f881d79392fd23b0608fff
Andre Assef <andreassef02@gmail.com>
2021-11-08 21:54:41 -0300

    Primeiro commit, projeto spring webflux

---
+++ src/test/java/com/example/assef/AssefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
